6433a9a72edd070ce9d8a08f5206f2aace00c97c
Sheikh Waqar <waqar.sheikh@aawebsolutions.net>
2020-08-06 16:07:43 +0500

    Quotation edit with add custom roll option

--- application/views/order_quotation/label_embellishment_print_service/label_emb_page/edit_main.php
+++ application/views/order_quotation/label_embellishment_print_service/label_emb_page/edit_main.php
@@ -291,7 +291,6 @@ if (isset($flag) && ($flag == 'order_detail' || $flag == 'quotation_detail' || $
                 <!--               --><?php //include('printing_process_and_product.php') ?>
 
                 <!-- Label Finishes & Embellishments & Cart Summary Starts -->
-<!--                <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>-->
                 <section>
                     <div class="row">
                         <div id="finish_content" class="col-md-9 col-xs-12">
